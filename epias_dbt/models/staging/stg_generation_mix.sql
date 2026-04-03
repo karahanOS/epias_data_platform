c0a7cfd22331e180a58526602559c9c347118135
Mehmet Karahan Çetinkaya <71176654+karahanOS@users.noreply.github.com>
2026-04-03 07:53:32 +0300

    sameee

--- epias_dbt/models/staging/stg_generation_mix.sql
+++ epias_dbt/models/staging/stg_generation_mix.sql
@@ -1,11 +1,11 @@
 SELECT
-    -- Join için bu kolonun mutlaka seçilmesi lazım
     CONCAT(CAST(date AS STRING), ' ', hour) as join_key,
     date,
     hour,
     total_generation,
     renewable_ratio,
     fossil_ratio,
-    year,
-    month
+    -- Partition kolonları yerine date üzerinden manuel extract yapıyoruz
+    EXTRACT(YEAR FROM date) as year,
+    EXTRACT(MONTH FROM date) as month
 FROM {{ source('epias_gold', 'gold_generation_mix_price_impact') }}
--- epias_dbt/models/staging/stg_price_spread.sql
+++ epias_dbt/models/staging/stg_price_spread.sql
@@ -7,5 +7,13 @@ SELECT
     smpUsd as smp_usd,
     price_spread_usd,
     system_direction,
-    season
+    -- Yine date üzerinden extract
+    EXTRACT(YEAR FROM date) as year,
+    EXTRACT(MONTH FROM date) as month,
+    CASE 
+        WHEN EXTRACT(MONTH FROM date) IN (12, 1, 2) THEN 'Kış'
+        WHEN EXTRACT(MONTH FROM date) IN (3, 4, 5) THEN 'İlkbahar'
+        WHEN EXTRACT(MONTH FROM date) IN (6, 7, 8) THEN 'Yaz'
+        ELSE 'Sonbahar'
+    END AS season
 FROM {{ source('epias_gold', 'gold_price_spread_analysis') }}
